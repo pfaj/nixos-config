3f0df85adf550948718bfd2fa0cd23da72f625f4
= <bdebiase2@gmail.com>
2023-08-02 12:08:27 -0400

    change mako margin

--- modules/home-manager/applications/mako.nix
+++ modules/home-manager/applications/mako.nix
@@ -11,7 +11,7 @@
         borderSize = 2;
         defaultTimeout = 5000;
         padding = "4,4,4,4";
-        margin = "4,4,4,4";
+        margin = "12,12,12,12";
         backgroundColor = "#00000080"; # 75% opacity
         borderColor = "#00000040"; # 25% opacity
     };
