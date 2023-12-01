7a81b2c38e4208a71b348c3d899800638c252204
ben <bdebiase2@gmail.com>
2023-11-30 19:00:58 -0500

    Add some packages to ags module

--- modules/home-manager/programs/ags.nix
+++ modules/home-manager/programs/ags.nix
@@ -1,9 +1,27 @@
-{
-  inputs,
-  pkgs,
-  ...
+{ inputs
+, pkgs
+, ...
 }: {
-  home.packages = [
-    inputs.ags.packages.${pkgs.system}.default
+  imports = [ inputs.ags.homeManagerModules.default ];
+
+  home.packages = with pkgs; [
+    sassc
+    swww
+    brightnessctl
+
+    hyprpicker
+    slurp
+    wf-recorder
+    wayshot
+    imagemagick
+    wl-gammactl
+    pavucontrol
+    swappy
+    (python311.withPackages(p: [ p.python-pam ]))
   ];
+
+  programs.ags = {
+    enable = true;
+    extraPackages = [ pkgs.libsoup_3 ];
+  };
 }
