35bacc90888dd1a883aeaf0e722a3dcc78207091
ben <bdebiase2@gmail.com>
2023-12-04 16:39:03 -0500

    Symlink ags config from aylur's dotfiles to ~/.config/ags

--- modules/home-manager/programs/ags.nix
+++ modules/home-manager/programs/ags.nix
@@ -1,10 +1,14 @@
-{ inputs
-, pkgs
-, ...
+{
+  inputs,
+  pkgs,
+  ...
 }: {
-  imports = [ inputs.ags.homeManagerModules.default ];
+  imports = [inputs.ags.homeManagerModules.default];
 
-  home.packages = with pkgs; [
+  xdg.configFile."ags".source = "${inputs.aylur-dotfiles}/ags";
+
+  home.
+    packages = with pkgs; [
     sassc
     swww
     brightnessctl
@@ -17,11 +21,11 @@
     wl-gammactl
     pavucontrol
     swappy
-    (python311.withPackages(p: [ p.python-pam ]))
+    (python311.withPackages (p: [p.python-pam]))
   ];
 
   programs.ags = {
     enable = true;
-    extraPackages = [ pkgs.libsoup_3 ];
+    extraPackages = [pkgs.libsoup_3];
   };
 }
