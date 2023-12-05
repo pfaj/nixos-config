2123ffab8488fa5d72b12abffd015b68884a5aa2
ben <bdebiase2@gmail.com>
2023-12-05 17:50:23 -0500

    Add declarative options for ags

--- modules/home-manager/programs/ags.nix
+++ modules/home-manager/programs/ags.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   pkgs,
   ...
@@ -7,22 +8,56 @@
 
   xdg.configFile."ags".source = "${inputs.aylur-dotfiles}/ags";
 
-  home.
-    packages = with pkgs; [
-    sassc
-    swww
-    brightnessctl
+  home = {
+    file.".cache/ags/options.json".text = ''
+      {
+        "theme.name": "Space",
+        "theme.icon": "",
+        "theme.scheme": "dark",
+        "theme.bg": "transparentize(#171717, 0.3)",
+        "theme.fg": "#eeeeee",
+        "theme.accent.gradient": "to right, $accent, lighten($accent, 6%)",
+        "theme.widget.bg": "$fg-color",
+        "theme.widget.opacity": 95,
+        "border.opacity": 92,
+        "border.width": 0,
+        "hypr.inactive_border": "rgba(333333ff)",
+        "bar.style": "normal",
+        "bar.separators": false,
+        "desktop.wallpaper.fg": "#fff",
+        "desktop.wallpaper.img": "${config.xdg.configHome}/nix/macos-wallpaper.jpg",
+        "desktop.screen_corners": true,
+        "desktop.drop_shadow": true,
+        "spacing": 8,
+        "padding": 8,
+        "radii": 16,
+        "theme.accent.accent": "$magenta",
+        "desktop.clock.enable": false,
+        "bar.icon": "",
+        "bar.flat_buttons": false,
+        "battery.show_percentage": false,
+        "font.font": "Roboto 10",
+        "font.size": 13,
+        "desktop.avatar": "${config.xdg.configHome}/nix/user-avatar.jpg"
+      }
+    '';
 
-    hyprpicker
-    slurp
-    wf-recorder
-    wayshot
-    imagemagick
-    wl-gammactl
-    pavucontrol
-    swappy
-    (python311.withPackages (p: [p.python-pam]))
-  ];
+    packages = with pkgs; [
+      sassc
+      swww
+      brightnessctl
+
+      hyprpicker
+      slurp
+      wf-recorder
+      wayshot
+      imagemagick
+      wl-gammactl
+      pavucontrol
+      swappy
+      (python311.withPackages (p: [p.python-pam]))
+    ];
+  };
 
   programs.ags = {
     enable = true;
