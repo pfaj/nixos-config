6f93b8deabdda6fb56067c2c1879d7fbf0502fd2
ben <bdebiase2@gmail.com>
2023-12-31 19:43:36 -0500

    Comment out ags cache config (temp fix)

--- modules/home-manager/programs/ags.nix
+++ modules/home-manager/programs/ags.nix
@@ -9,39 +9,39 @@
   xdg.configFile."ags".source = "${inputs.aylur-dotfiles}/ags";
 
   home = {
-    file.".cache/ags/options.json".text = ''
-      {
-        "theme.name": "Space",
-        "theme.icon": "",
-        "theme.scheme": "dark",
-        "theme.bg": "transparentize(#171717, 0.3)",
-        "theme.fg": "#eeeeee",
-        "theme.accent.gradient": "to right, $accent, lighten($accent, 6%)",
-        "theme.widget.bg": "$fg-color",
-        "theme.widget.opacity": 95,
-        "border.opacity": 92,
-        "border.width": 0,
-        "hypr.inactive_border": "rgba(333333ff)",
-        "bar.style": "normal",
-        "bar.separators": false,
-        "desktop.wallpaper.fg": "#fff",
-        "desktop.wallpaper.img": "${config.xdg.configHome}/nix/macos-wallpaper.jpg",
-        "desktop.screen_corners": true,
-        "desktop.drop_shadow": true,
-        "spacing": 8,
-        "padding": 8,
-        "radii": 16,
-        "theme.accent.accent": "$magenta",
-        "desktop.clock.enable": false,
-        "bar.icon": "",
-        "bar.flat_buttons": false,
-        "battery.show_percentage": false,
-        "font.font": "Roboto 10",
-        "font.size": 13,
-        "desktop.avatar": "${config.xdg.configHome}/nix/user-avatar.jpg",
-        "workspaces": 10
-      }
-    '';
+    #file.".cache/ags/options.json".text = ''
+    #  {
+    #    "theme.name": "Space",
+    #    "theme.icon": "",
+    #    "theme.scheme": "dark",
+    #    "theme.bg": "transparentize(#171717, 0.3)",
+    #    "theme.fg": "#eeeeee",
+    #    "theme.accent.gradient": "to right, $accent, lighten($accent, 6%)",
+    #    "theme.widget.bg": "$fg-color",
+    #    "theme.widget.opacity": 95,
+    #    "border.opacity": 92,
+    #    "border.width": 0,
+    #    "hypr.inactive_border": "rgba(333333ff)",
+    #    "bar.style": "normal",
+    #    "bar.separators": false,
+    #    "desktop.wallpaper.fg": "#fff",
+    #    "desktop.wallpaper.img": "${config.xdg.configHome}/nix/macos-wallpaper.jpg",
+    #    "desktop.screen_corners": true,
+    #    "desktop.drop_shadow": true,
+    #    "spacing": 8,
+    #    "padding": 8,
+    #    "radii": 16,
+    #    "theme.accent.accent": "$magenta",
+    #    "desktop.clock.enable": false,
+    #    "bar.icon": "",
+    #    "bar.flat_buttons": false,
+    #    "battery.show_percentage": false,
+    #    "font.font": "Roboto 10",
+    #    "font.size": 13,
+    #    "desktop.avatar": "${config.xdg.configHome}/nix/user-avatar.jpg",
+    #    "workspaces": 10
+    #  }
+    #'';
 
     packages = with pkgs; [
       sassc
