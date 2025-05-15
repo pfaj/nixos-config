f8d9d2557f421b6219e65ea922b4027c2f21dc97
pfaj <pfaj2004@gmail.com>
2025-05-15 11:48:41 -0400

    updates: updated quickshell and changes to tmux config

--- modules/home-manager/programs/tmux.nix
+++ modules/home-manager/programs/tmux.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs.tmuxPlugins; [
     catppuccin
+    onedark-theme
   ];
 
   programs.tmux = {
@@ -10,27 +11,27 @@
     escapeTime = 0;
     tmuxinator.enable = true;
     extraConfig = ''
-      run-shell '${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux'
+      run-shell '${pkgs.tmuxPlugins.onedark-theme}/share/tmux-plugins/onedark-theme/tmux-onedark-theme.tmux'
       # Plugins
-      set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_right_separator " "
-      set -g @catppuccin_window_middle_separator " █"
-      set -g @catppuccin_window_number_position "right"
-
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_window_default_text "#W"
-
-      set -g @catppuccin_window_current_fill "number"
-      set -g @catppuccin_window_current_text "#W"
-
-      set -g status-right-length 100
-      set -g @catppuccin_status_modules_right "#{E:@catppuccin_status_application}#{E:@catppuccin_status_session}"
-      set -g @catppuccin_status_left_separator  " "
-      set -g @catppuccin_status_right_separator ""
-      set -g @catppuccin_status_fill "icon"
-      set -g @catppuccin_status_connect_separator "no"
-
-      set -g @catppuccin_directory_text "#{pane_current_path}"
+      # set -g @catppuccin_window_left_separator ""
+      # set -g @catppuccin_window_right_separator " "
+      # set -g @catppuccin_window_middle_separator " █"
+      # set -g @catppuccin_window_number_position "right"
+      #
+      # set -g @catppuccin_window_default_fill "number"
+      # set -g @catppuccin_window_default_text "#W"
+      #
+      # set -g @catppuccin_window_current_fill "number"
+      # set -g @catppuccin_window_current_text "#W"
+      #
+      # set -g status-right-length 100
+      # set -g @catppuccin_status_modules_right "#{E:@catppuccin_status_application}#{E:@catppuccin_status_session}"
+      # set -g @catppuccin_status_left_separator  " "
+      # set -g @catppuccin_status_right_separator ""
+      # set -g @catppuccin_status_fill "icon"
+      # set -g @catppuccin_status_connect_separator "no"
+      #
+      # set -g @catppuccin_directory_text "#{pane_current_path}"
 
       # Other settings
       set-option -g status-position top
