554687113889ae037ce21cde026d8148a42698f1
pfaj <pfaj2004@gmail.com>
2025-09-08 11:50:51 -0400

    virtualization: changed the software used for virtualization

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -1,5 +1,13 @@
-{username, ...}: {
+{ username, ... }:
+{
   # virtualization
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = ["${username}"];
+  # virtualisation.virtualbox.host.enable = true;
+  # users.users.${username}.extraGroups = ["vboxusers"];
+  programs.virt-manager.enable = true;
+
+  users.groups.libvirtd.members = [ "${username}" ];
+
+  virtualisation.libvirtd.enable = true;
+
+  virtualisation.spiceUSBRedirection.enable = true;
 }
